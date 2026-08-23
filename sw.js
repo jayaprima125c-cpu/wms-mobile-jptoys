f708e3f66bbcff4f0f9f82336fa58f208aa6ea5e
jayaprima125c-cpu <jayaprima125c@gmail.com>
2026-08-23 20:25:02 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for WMS Mobile Scanner PWA - Jaya Prima Toys
-const CACHE_NAME = 'wms-jptoys-v3.7';
+const CACHE_NAME = 'wms-jptoys-v3.9';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
